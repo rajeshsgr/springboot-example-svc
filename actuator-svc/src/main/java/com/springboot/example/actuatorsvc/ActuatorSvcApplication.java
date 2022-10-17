a951e74736415c4782a885b97ca8cdb2be108948
rajesh.sgr <rajesh.sgr@gmail.com>
2022-10-16 21:31:07 -0500

    Sample code for Spring Actuator Service

---
+++ actuator-svc/src/main/java/com/springboot/example/actuatorsvc/ActuatorSvcApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example.actuatorsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorSvcApplication.class, args);
+    }
+
+}
---
+++ actuator-svc/src/test/java/com/springboot/example/actuatorsvc/ActuatorSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.example.actuatorsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
